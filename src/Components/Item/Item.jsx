7fd6e4f690b55cee5c7dfa69b7dd496559289e6c
franco <marcuzzi7franco@gmail.com>
2022-08-03 08:53:13 -0300

    agregado archivos css

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Item.css";
 
 export const Item = ({ item }) => {
   return (
-    <div>
+    <div className="card">
       <h3>{item.tittle}</h3>
-      <img src={item.pictureUrl} />
+      <img src={item.pictureUrl} alt="not found" />
       <h4>{item.description}</h4>
       <h4> $ {item.price}</h4>
     </div>
--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Item } from "../Item/Item";
+import "./ItemList.css";
 
 export const ItemList = ({ listItem }) => {
   return (
-    <div>
+    <div className="containerCards">
       {listItem.map((item) => (
         <div key={item.id}>
           <Item item={item} />
